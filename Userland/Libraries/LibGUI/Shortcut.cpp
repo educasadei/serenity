8be0d17cafaffb57d601ca8242e4c0e0d6699853
Eduardo Casadei <educasadei@gmail.com>
2026-04-23 10:23:23 -0400

    LibGUI: Make Shortcut bindable to a character
    Add the ability for a Shortcut to bind to a character instead of just a
    key code, allowing it to be more keyboard layout agnostic.

--- Userland/Libraries/LibGUI/Shortcut.cpp
+++ Userland/Libraries/LibGUI/Shortcut.cpp
@@ -10,8 +10,23 @@
 #include <AK/StringBuilder.h>
 #include <AK/Vector.h>
 #include <LibGUI/Shortcut.h>
+#include <LibUnicode/CharacterTypes.h>
+#include <string.h>
 
 namespace GUI {
+
+Shortcut::Shortcut(u8 modifiers, u32 code_point)
+    : m_type(Type::Keyboard)
+    , m_modifiers(modifiers)
+    , m_code_point(Unicode::to_unicode_uppercase(code_point))
+{
+}
+
+Shortcut::Shortcut(u32 code_point)
+    : m_type(Type::Keyboard)
+    , m_code_point(Unicode::to_unicode_uppercase(code_point))
+{
+}
 
 bool Shortcut::matches(KeyEvent const& event) const
 {
@@ -21,6 +36,8 @@ bool Shortcut::matches(KeyEvent const& event) const
         return false;
 
     if (m_keyboard_key != KeyCode::Key_Invalid && m_keyboard_key == event.key())
+        return true;
+    if (m_code_point != 0 && m_code_point == Unicode::to_unicode_uppercase(event.code_point()))
         return true;
 
     return false;
@@ -38,34 +55,40 @@ bool Shortcut::matches(MouseEvent const& event) const
 
 ByteString Shortcut::to_byte_string() const
 {
-    Vector<ByteString, 8> parts;
+    Vector<StringView, 6> parts;
 
     if (m_modifiers & Mod_Ctrl)
-        parts.append("Ctrl");
+        parts.append("Ctrl"sv);
     if (m_modifiers & Mod_Shift)
-        parts.append("Shift");
+        parts.append("Shift"sv);
     if (m_modifiers & Mod_Alt)
-        parts.append("Alt");
+        parts.append("Alt"sv);
     if (m_modifiers & Mod_AltGr)
-        parts.append("AltGr");
+        parts.append("AltGr"sv);
     if (m_modifiers & Mod_Super)
-        parts.append("Super");
-
-    if (m_type == Type::Keyboard) {
-        if (auto* key_name = key_code_to_string(m_keyboard_key))
-            parts.append(key_name);
-        else
-            parts.append("(Invalid)");
-    } else {
-        if (m_mouse_button != MouseButton::None)
-            parts.append(ByteString::formatted("Mouse {}", mouse_button_to_string(m_mouse_button)));
-        else
-            parts.append("(Invalid)");
-    }
+        parts.append("Super"sv);
 
     StringBuilder builder;
-    builder.join('+', parts);
-    return builder.to_byte_string();
+
+    if (m_type == Type::Keyboard) {
+        if (m_code_point != 0)
+            builder.append_code_point(m_code_point);
+        else if (auto* key_name = key_code_to_string(m_keyboard_key))
+            builder.append(key_name, strlen(key_name));
+        else
+            builder.append("(Invalid)"sv);
+    } else {
+        if (m_mouse_button != MouseButton::None)
+            builder.appendff("Mouse {}", mouse_button_to_string(m_mouse_button));
+        else
+            builder.append("(Invalid)"sv);
+    }
+
+    parts.append(builder.string_view());
+
+    StringBuilder joiner;
+    joiner.join('+', parts);
+    return joiner.to_byte_string();
 }
 
 }
--- Userland/Libraries/LibGUI/Shortcut.h
+++ Userland/Libraries/LibGUI/Shortcut.h
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018-2020, Andreas Kling <kling@serenityos.org>
  * Copyright (c) 2022, Geordie Hall <me@geordiehall.com>
+ * Copyright (c) 2026, Eduardo Casadei <educasadei@gmail.com>
  *
  * SPDX-License-Identifier: BSD-2-Clause
  */
@@ -16,6 +17,10 @@ namespace GUI {
 class Shortcut {
 public:
     Shortcut() = default;
+
+    Shortcut(u8 modifiers, u32 code_point);
+    Shortcut(u32 code_point);
+
     Shortcut(u8 modifiers, KeyCode key)
         : m_type(Type::Keyboard)
         , m_modifiers(modifiers)
@@ -48,13 +53,19 @@ public:
 
     ByteString to_byte_string() const;
     Type type() const { return m_type; }
-    bool is_valid() const { return m_type == Type::Keyboard ? (m_keyboard_key != Key_Invalid) : (m_mouse_button != MouseButton::None); }
+    bool is_valid() const { return m_type == Type::Keyboard ? ((m_keyboard_key != Key_Invalid) != (m_code_point != 0)) : (m_mouse_button != MouseButton::None); }
     u8 modifiers() const { return m_modifiers; }
 
     KeyCode key() const
     {
         VERIFY(m_type == Type::Keyboard);
         return m_keyboard_key;
+    }
+
+    u32 code_point() const
+    {
+        VERIFY(m_type == Type::Keyboard);
+        return m_code_point;
     }
 
     MouseButton mouse_button() const
@@ -68,13 +79,14 @@ public:
 
     bool operator==(Shortcut const& other) const
     {
-        return m_modifiers == other.m_modifiers && m_type == other.m_type && m_keyboard_key == other.m_keyboard_key && m_mouse_button == other.m_mouse_button;
+        return m_modifiers == other.m_modifiers && m_type == other.m_type && m_keyboard_key == other.m_keyboard_key && m_code_point == other.m_code_point && m_mouse_button == other.m_mouse_button;
     }
 
 private:
     Type m_type { Type::Keyboard };
     u8 m_modifiers { 0 };
     KeyCode m_keyboard_key { KeyCode::Key_Invalid };
+    u32 m_code_point { 0 };
     MouseButton m_mouse_button { MouseButton::None };
 };
 
@@ -86,11 +98,12 @@ template<>
 struct Traits<GUI::Shortcut> : public DefaultTraits<GUI::Shortcut> {
     static unsigned hash(const GUI::Shortcut& shortcut)
     {
-        auto base_hash = pair_int_hash(shortcut.modifiers(), (u32)shortcut.type());
+        auto hash = pair_int_hash(shortcut.modifiers(), (u32)shortcut.type());
         if (shortcut.type() == GUI::Shortcut::Type::Keyboard) {
-            return pair_int_hash(base_hash, shortcut.key());
+            hash = pair_int_hash(hash, shortcut.code_point());
+            return pair_int_hash(hash, shortcut.key());
         } else {
-            return pair_int_hash(base_hash, shortcut.mouse_button());
+            return pair_int_hash(hash, shortcut.mouse_button());
         }
     }
 };
